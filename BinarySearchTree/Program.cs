b6cd38ed0f9dd985bbd905860e802e0b058548d1
Nilesh9492 <nilesh94041@gmail.com>
2021-10-23 09:56:23 +0530

    create a tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -12,6 +12,20 @@ namespace BinarySearchTree
             binary.Insert(30);
             binary.Insert(70);
             binary.View();
+
+            binary.Insert(30);
+            binary.Insert(70);
+            binary.Insert(22);
+            binary.Insert(40);
+            binary.Insert(11);
+            binary.Insert(3);
+            binary.Insert(16);
+            binary.Insert(60);
+            binary.Insert(95);
+            binary.Insert(65);
+            binary.Insert(63);
+            binary.Insert(67);
+            binary.View();
         }
     }
 }
