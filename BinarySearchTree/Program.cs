823737e5385e121724f98dacaf344fcb681abf4b
Nilesh9492 <nilesh94041@gmail.com>
2021-10-23 09:14:59 +0530

    Creating BST

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search progarm");
+            BinarySearch binary = new BinarySearch();
+            binary.Insert(56);
+            binary.Insert(30);
+            binary.Insert(70);
+            binary.View();
+        }
+    }
+}
---
+++ BinarySearchTree/TreeNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class TreeNode
+    {
+        public int data;
+        public TreeNode leftNode;
+        public TreeNode rightNode;
+        public TreeNode(int data)
+        {
+            this.data = data;
+            leftNode = null;
+            rightNode = null;
+        }
+        public void Insert(int data)
+        {
+            int rootData = this.data;
+            if (data >= rootData)
+            {
+                if (rightNode == null)
+                {
+                    rightNode = new TreeNode(data);
+                }
+                else
+                {
+                    rightNode.Insert(data);
+                }
+            }
+            else
+            {
+                if (leftNode == null)
+                {
+                    leftNode = new TreeNode(data);
+                }
+                else
+                {
+                    leftNode.Insert(data);
+                }
+            }
+        }
+        
+        public void OrderTraversal()
+        {
+            if (leftNode != null)
+            {
+                leftNode.OrderTraversal();
+            }
+            Console.Write(data + " ");
+            if (rightNode != null)
+            {
+                rightNode.OrderTraversal();
+            }
+        }
+    }
+}
